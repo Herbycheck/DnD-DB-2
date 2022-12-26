baba128f98f05c916271ed5f55f652d482ec05db
Tobias Herbitschek <tobias.herbitschek@gmail.com>
2022-12-26 11:44:37 +0100

    Add database connection verification

--- modules/db.example.js
+++ modules/db.example.js
@@ -1,12 +1,19 @@
 const db = require('knex')({
-    client: 'pg',
-    connection: {
-      host : 'example.com',
-      port : 5432,
-      user : 'postgres',
-      password : 'xxxxxxxxxxx',
-      database : 'dndb'
-    }
-  });
+	client: 'pg',
+	connection: {
+		host: 'example.com',
+		port: 5432,
+		user: 'postgres',
+		password: 'xxxxxxxxxxx',
+		database: 'dndb'
+	}
+});
 
-  module.exports = {db}
+db.raw("SELECT 1").then(() => {
+	console.log("PostgreSQL connected");
+})
+	.catch((e) => {
+		console.log("PostgreSQL not connected");
+		console.error(e);
+	});
+module.exports = { db }
